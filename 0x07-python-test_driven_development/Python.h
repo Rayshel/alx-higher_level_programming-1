503f01242b840b023cb57373148cc1f78de509b6
Rachael Ijeoma <ijeomarachel01@gmail.com>
2023-01-06 23:50:54 +0100

    tasks

---
+++ 0x07-python-test_driven_development/Python.h
@@ -0,0 +1,14 @@
+#ifndef PYTHON_H
+#define PYTHON_H
+
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_python_string - prints basic info about python strings
+ * @p: A PyObject string object
+ */
+
+void print_python_string(PyObject *p);
+
+#endif /* PYTHON_H */
