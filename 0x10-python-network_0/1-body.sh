9838f3b3a4ab76901584474807553e1f3b9262c8
Rachael Ijeoma <ijeomarachel01@gmail.com>
2023-03-30 23:21:16 +0100

    Update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# send a GET request to an URL with curl, and display the body of the response
-curl -sL "$1"
+# sends GET request to the URL and displays the body of the response
+curl -sfL "$1" -X GET
